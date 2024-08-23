d0f739853a4347c31b9879b00d7b5bfd02786823
KirVoz <k.a.voz97@gmail.com>
2024-08-23 07:19:24 +0400

    utils

---
+++ utils.c
@@ -0,0 +1,15 @@
+# include "minishell.h"
+
+bool    is_space(char c)
+{
+    if (c == ' ' || c == '\t' || c == '\n' || c == '\v' || c == '\f' || c == '\r')
+        return (1);
+    return (0);
+}
+
+bool    is_digit_or_alpha(char c)
+{
+    if ((c >= '0' && c <= '9') || (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+        return (1);
+    return (0);
+}
